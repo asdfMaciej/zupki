05b689141d19f02c658362f12ce2c18ceb03fed0
asdfMaciej <maciej.kaszkowiak@gmail.com>
2017-06-21 15:46:50 +0200

    Nauka django - zobaczmy co z tego wyniknie

---
+++ zupki/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for zupki project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "zupki.settings")
+
+application = get_wsgi_application()
