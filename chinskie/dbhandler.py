659128477153f6de9d13a00e49b5c5298bdd3a46
asdfMaciej <maciej.kaszkowiak@gmail.com>
2017-06-26 02:40:23 +0200

    What a mess

--- chinskie/dbhandler.py
+++ chinskie/dbhandler.py
@@ -3,6 +3,42 @@ import re
 
 def sanitize(_str): return re.sub('[^a-zA-Z0-9ążźćłńóśĄŻŹĆŁŃÓŚ \n\.]', '', _str)
 
+
+def essential_to_dict(tple):
+    def ar_to_dzn(ar):
+        dzn = ''
+        dzn = 'Poniedziałek' if ar[3] else dzn
+        dzn = 'Wtorek' if ar[4] else dzn
+        dzn = 'Środa' if ar[5] else dzn
+        dzn = 'Czwartek' if ar[6] else dzn
+        dzn = 'Piątek' if ar[7] else dzn
+        return dzn
+
+    retdict = {'Poniedziałek': {}, 'Wtorek': {}, 'Środa': {}, 'Czwartek': {}, 'Piątek': {}}
+    for it in range(9):
+        for key, value in retdict.items():
+            retdict[key][it] = []
+
+    for lsn in tple:
+        dzn = ar_to_dzn(lsn)
+        prd = int(lsn[8])
+        retdict[dzn][prd] = lsn
+
+    return retdict
+
+def actually_screw_django(dct):
+    dictstoocomplicated = []
+    n = 0
+    for key, value in dct.items():
+        dictstoocomplicated.append([key])
+        for key, value in value.items():
+            str_val = ""
+            if value:
+                str_val = '<i>'+value[0]+'<br/>'+value[1]+'<br/></i><b>'+value[11]+'</b><br/>'+value[2]
+            dictstoocomplicated[n].append(str_val)
+        n += 1
+    return dictstoocomplicated
+
 def get_query(query):
     conn = sql3.connect('baza.db')
     cur = conn.cursor()
@@ -14,10 +50,19 @@ def get_all(parameter, _str):
     return get_query('SELECT * from jednostki_lekcyjne WHERE '+sanitize(parameter)+'="'+sanitize(_str)+'";')
 
 def get_essential(parameter, _str):
-    return get_query('SELECT class, classroom, teacher, d_monday, d_tuesday, d_wednesday, d_thursday, d_friday, period, period_end, period_start, subject from jednostki_lekcyjne WHERE ' + sanitize(parameter) + '="' + sanitize(_str) + '";')
+    return actually_screw_django(
+        essential_to_dict(
+            get_query(
+                'SELECT class, classroom, teacher, d_monday, d_tuesday, d_wednesday, d_thursday, d_friday, period,' +
+                'period_end, period_start, subject from jednostki_lekcyjne WHERE ' +
+                sanitize(parameter) + '="' + sanitize(_str) + '";'
+            )
+        )
+    )
 
 def get_column_names():
     return list(zip(*get_query('PRAGMA table_info(jednostki_lekcyjne);')))[1]
 
 def get_teachers():
     return get_query('SELECT DISTINCT teacher_id, teacher from jednostki_lekcyjne;')
+
--- chinskie/views.py
+++ chinskie/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from chinskie import dbhandler
+from django.template import loader
 
 def index(request):
     return HttpResponse(str(dbhandler.get_all('class', '3f')))
@@ -12,7 +13,12 @@ def class_get(request, klasa):
     return HttpResponse(str(dbhandler.get_essential('class', klasa)))
 
 def teacher_get(request, nauczyciel):
-    return HttpResponse(str(dbhandler.get_essential('teacher', nauczyciel)))
+    japierdole = dbhandler.get_essential('teacher', nauczyciel)
+    context = {
+        'essential': japierdole,
+    }
+    template = loader.get_template('zupki/tabelka.html')
+    return HttpResponse(template.render(context, request))
 
 def teacher_list(request):
     return HttpResponse(str(dbhandler.get_teachers()))
