8c5713b11753699a5b606a282254fe57a6249c93
asdfMaciej <maciej.kaszkowiak@gmail.com>
2017-06-26 16:16:52 +0200

    Styling changes etc

--- chinskie/dbhandler.py
+++ chinskie/dbhandler.py
@@ -3,6 +3,11 @@ import re
 
 def sanitize(_str): return re.sub('[^a-zA-Z0-9ążźćłńóśĄŻŹĆŁŃÓŚ \n\.]', '', _str)
 
+def cCol(my_hex):
+    r, g, b = tuple(int(my_hex.lstrip('#')[i:i + 2], 16) for i in (0, 2, 4))
+    a = 1 - float((0.299 * r + 0.587 * g + 0.114 * b) / 255);
+    return '#000000' if a < 0.5 else '#FFFFFF'
+    #return '#'+''.join(comp)
 
 def essential_to_dict(tple):
     def ar_to_dzn(ar):
@@ -22,7 +27,7 @@ def essential_to_dict(tple):
     for lsn in tple:
         dzn = ar_to_dzn(lsn)
         prd = int(lsn[8])
-        retdict[dzn][prd] = lsn
+        retdict[dzn][prd].append(lsn)
 
     return retdict
 
@@ -31,10 +36,20 @@ def actually_screw_django(dct):
     n = 0
     for key, value in dct.items():
         dictstoocomplicated.append([key])
-        for key, value in value.items():
+        for key, val in value.items():
             str_val = ""
-            if value:
-                str_val = '<i>'+value[0]+'<br/>'+value[1]+'<br/></i><b>'+value[11]+'</b><br/>'+value[2]+'<br/>'
+            if val:
+                use_table = len(val) > 1
+                #if use_table:
+                str_val = '<table class="in" bgcolor="'+val[0][12]+'"style="width: 100%;display: block;color:'+cCol(val[0][12])+';">'
+                for value in val:
+                    #if use_table:
+                    str_val += '<tr class="in"><td class="in">'
+                    str_val += '<i>'+value[0]+'<br/>'+value[1]+'<br/></i><b>'+value[11]+'</b><br/>'+value[2]+'<br/>'
+                    #if use_table:
+                    str_val += '</td></tr>'
+                #if use_table:
+                str_val += '</table>'
             dictstoocomplicated[n].append(str_val)
         n += 1
     return dictstoocomplicated
@@ -54,7 +69,7 @@ def get_essential(parameter, _str):
         essential_to_dict(
             get_query(
                 'SELECT class, classroom, teacher, d_monday, d_tuesday, d_wednesday, d_thursday, d_friday, period,' +
-                'period_end, period_start, subject from jednostki_lekcyjne WHERE ' +
+                'period_end, period_start, subject, subject_color from jednostki_lekcyjne WHERE ' +
                 sanitize(parameter) + '="' + sanitize(_str) + '";'
             )
         )
