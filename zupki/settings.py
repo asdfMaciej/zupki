c595bac753407746844f8b29202d0033a80a5cd6
asdfMaciej <maciej.kaszkowiak@gmail.com>
2017-09-05 22:28:30 +0200

    Update for this school year

--- zupki/settings.py
+++ zupki/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'plg3u$u6rbl)^5a@j!(_t_=gzmla^jyk7$tv@$m$b!f4yjt+o5'  # you cant tell me how to live x2
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False  # nah jk maybe it briefly will
+DEBUG = True  # nah jk maybe it briefly will
 
 ALLOWED_HOSTS = ['*', 'localhost', '127.0.0.1']
 
